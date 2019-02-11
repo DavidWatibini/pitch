4c2abe6dabda177da6d7564f29890987ec1bc0f0
Watinguyaz <watimakhanu@gmail.com>
2019-02-11 19:03:46 +0300

    register file set

--- app/__init__.py
+++ app/__init__.py
@@ -2,22 +2,15 @@ from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+# from . import login_manager
 from flask_login import LoginManager
-from .main import main as main_blueprint
-from . import login_manager
 
-from .auth import auth as auth_blueprint
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
 
 def create_app(config_name):
 
@@ -31,6 +24,8 @@ def create_app(config_name):
     db.init_app(app)
 
     login_manager.init_app(app)
+    from .auth import auth as auth_blueprint
+    from .main import main as main_blueprint
 
 
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
