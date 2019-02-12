90ad94fc55f6fd2c1bfd772013697890d6bdf905
Watinguyaz <watimakhanu@gmail.com>
2019-02-12 07:47:19 +0300

    profile page set

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from flask_login import login_required
+from ..models import Reviews, User
 
 @main.route('/')
 def index():
@@ -11,3 +12,12 @@ def index():
 # @main.route('/***/pitch/new', methods = ['GET','POST'])
 # @login_required
 # def new_pitch():
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
