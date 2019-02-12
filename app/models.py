634beb7a8a9a56b75690cc1dd3a9dfa0e33f2ede
Watinguyaz <watimakhanu@gmail.com>
2019-02-12 07:32:43 +0300

    migrations updated for bio and profile picture

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from . import login_manager
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-    
+
 class User(UserMixin,db.Model):
 
     __tablename__ = 'users'
@@ -15,8 +15,9 @@ class User(UserMixin,db.Model):
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     email = db.Column(db.String(255),unique = True,index = True)
     password_hash = db.Column(db.String(255))
-
     pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
 
     @property
     def password(self):
