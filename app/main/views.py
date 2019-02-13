6fbc1b3d07622e37d2a38de823e3e408653f27a1
Watinguyaz <watimakhanu@gmail.com>
2019-02-13 10:59:37 +0300

    add new pitch set

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template,request,redirect,url_for,abort
 from . import main
 from flask_login import login_required
 from ..models import User,Feedback,Pitch
-from .forms import FeedbackForm
+from .forms import FeedbackForm,PitchForm
 from .. import db
 
 # landing
